8ac5eaec572abff127b055db04b2766cf5afd065
ValKobilaski <34190443+ValKobilaski@users.noreply.github.com>
2017-12-02 16:02:11 -0500

    remote-tracking branch 'origin/master' Centered images of dinning halls # Conflicts: #	app/templates/index.html

--- app/data.py
+++ app/data.py
@@ -20,13 +20,111 @@ halls = [
         "people": 0
     },
     {
+        "Name":"Jean-Royce",
+        "lat":44.224243,
+        "lng": -76.515335,
+        "infoBox":"",
+        "icon": icons["green"],
+        "people":0
+    },
+    {
         "name": "Lazy Scholar",
         "lat": 44.225633,
         "lng": -76.498783,
         "infobox":"",
         "icon": icons["blue"],
         "people": 0
-    }
+    },
+    {
+        "Name":"Location 21",
+        "lat":44.223575,
+        "lng":-76.498995,
+        "infoBox":"",
+        "icon": icons["green"],
+        "people":0
+    },
+    {
+        "Name":"The Arc",
+        "lat":44.229325,
+        "lng":-76.494231,
+        "infoBox":"",
+        "icon": icons["green"],
+        "people":0
+    },
+    {
+        "Name":"The JDUC",
+        "lat":44.228472,
+        "lng":-76.494675,
+        "infoBox":"",
+        "icon": icons["green"],
+        "people":0
+    },
+    {
+        "Name":"Tim Horton's (BioSci)",
+        "lat":44.226444,
+        "lng": -76.491273,
+        "infoBox":"",
+        "icon": icons["green"],
+        "people":0
+    },
+    {
+        "Name": "Tim Horton's (BioSci)",
+        "lat": 44.226444,
+        "lng": -76.491273,
+        "infoBox": "",
+        "icon": icons["green"],
+        "people": 0
+    },
+    {
+        "Name": "MC Square",
+        "lat": 44.226615,
+        "lng": -76.497020,
+        "infoBox": "",
+        "icon": icons["green"],
+        "people": 0
+    },
+    {
+        "Name": "Market Street",
+        "lat": 44.224566,
+        "lng": -76.491620,
+        "infoBox": "",
+        "icon": icons["green"],
+        "people": 0
+    },
+    {
+        "Name": "Market Street",
+        "lat": 44.224566,
+        "lng": -76.491620,
+        "infoBox": "",
+        "icon": icons["green"],
+        "people": 0
+    },
+    {
+        "Name": "Recharge",
+        "lat": 44.225394,
+        "lng": -76.491291,
+        "infoBox": "",
+        "icon": icons["green"],
+        "people": 0
+    },
+    {
+        "Name": "Goodes Hall",
+        "lat": 44.228306,
+        "lng": -76.497559,
+        "infoBox": "",
+        "icon": icons["green"],
+        "people": 0
+    },
+{
+        "Name": "Gord's Cafe",
+        "lat": 44.223129,
+        "lng": -76.500764,
+        "infoBox": "",
+        "icon": icons["green"],
+        "people": 0
+    },
+
+
 ]
 lat = 44.226122
 lng = -76.495110
