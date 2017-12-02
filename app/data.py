32c2c9514100f2053f23ff6d8a5d20944d61826c
Dominic <TeamDman9201@gmail.com>
2017-12-02 15:24:52 -0500

    Lot of work for nothing YEET

--- app/data.py
+++ app/data.py
@@ -1,9 +1,9 @@
 halls = [
     {
-        "name": "lenny",
+        "name": "Lenny",
         "lat": 44.224267,
         "lng": -76.500690,
-        "infobox": "lenny hallll",
+        "infobox": "",
         "icon": "http://maps.google.com/mapfiles/ms/icons/green-dot.png",
         "people": 0
     },
@@ -11,10 +11,17 @@ halls = [
         "name": "Ban Righ",
         "lat": 44.224628,
         "lng": -76.496264,
-        "infobox": "Yeet burgers",
+        "infobox": "",
         "icon": "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
         "people": 0
     }
 ]
 lat = 44.226122
 lng = -76.495110
+
+
+def update():
+    for hall in halls:
+        hall["infobox"] = "%d people<br><a href=''>Check In</a>" % hall["people"]
+
+
