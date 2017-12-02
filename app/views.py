78840863a1b213dbe48283e694997e83097d5bda
ValerijKobilaski <34190089+ValerijKobilaski@users.noreply.github.com>
2017-12-02 12:15:53 -0500

    ben wuz heer

--- app/views.py
+++ app/views.py
@@ -1,22 +1,23 @@
 from flask import render_template
 from app import app
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'nickname': 'friend'}
     drinks = [
         {
-            'name':'cola',
-            'colour':'red'
+            'name': 'cola',
+            'colour': 'red'
         },
         {
-            'name':'drank',
-            'colour':'purple'
+            'name': 'drank',
+            'colour': 'purple'
         },
         {
-            'name':'Pop',
-            'colour':'Orange'
+            'name': 'Pop',
+            'colour': 'Orange'
         }
     ]
     return render_template('index.html', title='Home', user=user, drinks=drinks)
@@ -25,3 +26,8 @@ def index():
 @app.route('/dom')
 def dom():
     return render_template('dom.html')
+
+
+@app.route('/ben')
+def ben():
+    return render_template('Ben.html')
