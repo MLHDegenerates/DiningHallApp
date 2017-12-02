a53a2d6cf3daa2d177a0608b5f39d2596d3354e0
Dominic <TeamDman9201@gmail.com>
2017-12-02 15:31:18 -0500

    Added lazy

--- app/data.py
+++ app/data.py
@@ -1,10 +1,14 @@
+icons = {
+    "green":"http://maps.google.com/mapfiles/ms/icons/green-dot.png",
+    "blue":"http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+}
 halls = [
     {
         "name": "Lenny",
         "lat": 44.224267,
         "lng": -76.500690,
         "infobox": "",
-        "icon": "http://maps.google.com/mapfiles/ms/icons/green-dot.png",
+        "icon": icons["green"],
         "people": 0
     },
     {
@@ -12,16 +16,18 @@ halls = [
         "lat": 44.224628,
         "lng": -76.496264,
         "infobox": "",
-        "icon": "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+        "icon": icons["green"],
+        "people": 0
+    },
+    {
+        "name": "Lazy Scholar",
+        "lat": 44.225633,
+        "lng": -76.498783,
+        "infobox":"",
+        "icon": icons["blue"],
         "people": 0
     }
 ]
 lat = 44.226122
 lng = -76.495110
-
-
-def update():
-    for hall in halls:
-        hall["infobox"] = "%d people<br><a href=''>Check In</a>" % hall["people"]
-
 
